4947a2511dd4c882689e7f658c72e12c79c97675
Tanvir-Gahunia <gahunia123@gmail.com>
2025-02-18 16:23:45 -0500

    code cleanup

--- matrix.h
+++ matrix.h
@@ -6,13 +6,12 @@ class Matrix
     std::vector<std::vector<float> > data;
 public:
     Matrix() = default;
+    Matrix(const std::vector<std::vector<float> >&);
+    Matrix(const int rows, const int cols);
+    Matrix& operator=(const Matrix&) = default;
     Matrix transpose() const;
     Matrix row_to_matrix(uint) const;
     Matrix sub_matrix(uint, uint, uint, uint) const;
-    Matrix(const int rows, const int cols);
-    Matrix(const std::vector<std::vector<float> >&);
-    Matrix& operator=(const Matrix&) = default;
-    void print() const;
     Matrix operator+(const Matrix&) const;
     Matrix operator-(const Matrix&) const;
     Matrix operator*(const Matrix&) const;
@@ -20,12 +19,13 @@ public:
     Matrix element_wise_product(const Matrix&) const;
     Matrix sumRows() const;
     Matrix broadcast(uint) const;
-    void rand(float, float);
-    void rand_he(uint);
     float& at(uint i, uint j);
     const float& at(uint i, uint j) const;
     int rows() const;
     int cols() const;
+    void rand(float, float);
+    void rand_he(uint);
+    void print() const;
     void activation(float (*activation_func)(float));
 };
 #endif
